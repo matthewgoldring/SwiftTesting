213b4975640be2d06ce946fddb0a0f73c23e4e7b
matt <matthewgoldring@gmail.com>
2024-01-20 21:42:33 +0000

    Initial Commit

---
+++ NewSimpleWatchToPhone/NewSimpleWatchToPhoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewSimpleWatchToPhoneApp.swift
+//  NewSimpleWatchToPhone
+//
+//  Created by Matthew on 20/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct NewSimpleWatchToPhoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
